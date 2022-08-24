c958c98ada392a937d9a4a75f880a053a546c6a7
cyberbex <cyberbex@gmail.com>
2022-08-24 13:28:05 -0300

    pronto

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,46 @@
-import 'package:fire_base_auth/widget_tree.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'Home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const Home());
-}
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  FirebaseAuth auth = FirebaseAuth.instance;
+  String email = 'gamba@gmail.com';
+  String senha = 'gamba88';
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.orange),
-      home: const WidgetTree(),
-    );
-  }
+  //criar usuário
+  /* auth
+      .createUserWithEmailAndPassword(email: email, password: senha)
+      .then((firebaseUser) {
+    print('novo usuário cadastrado com sucesso $firebaseUser.user.email');
+  }).catchError((erro) {
+    print("novo usuario erro $erro");
+  }); */
+
+  //deslogar usuário
+  //auth.signOut();
+
+//saber se o usuário está logado ou não
+  /*  User? usuarioAtual = auth.currentUser;
+  if (usuarioAtual != null) {
+    print("Usuário atual logado email: $usuarioAtual.email");
+  } else {
+    print("usuário está deslogado");
+  } */
+
+  auth
+      .signInWithEmailAndPassword(email: email, password: senha)
+      .then((firebaseUser) {
+    print('Logar usuário sucesso!! email:${firebaseUser.user?.email}');
+  }).catchError((erro) {
+    print('Logar usuário error $erro');
+  });
+
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
 }
